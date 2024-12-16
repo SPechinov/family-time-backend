9d8f64b2058e8d263548c503b06992f302e1611e
Sergei Pechinov <spechinov@gmail.com>
2024-12-16 22:07:35 +0100

    Check CI/CD

--- internal/api/rest/routes/auth/handlers.go
+++ internal/api/rest/routes/auth/handlers.go
@@ -48,7 +48,7 @@ func (c *Controller) registration(props base_controller.Props[RegistrationDTO]) 
 		return err
 	}
 
-	return props.EchoCTX.NoContent(http.StatusGone)
+	return props.EchoCTX.NoContent(http.StatusAccepted)
 }
 
 func (c *Controller) registrationConfirm(props base_controller.Props[RegistrationConfirmDTO]) error {
